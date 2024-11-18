a33852dbdf907359c2bedb77c626b6e2f24ad446
XabibullayevAbror <abrorboiiixabibullayev@gmail.com>
2024-11-18 18:31:43 +0500

    Add files via upload

---
+++ scripti.js
@@ -0,0 +1,19 @@
+// 1 SAVOL
+// let a = prompt()
+
+// 2 SAVOL
+// let a = 1
+// let b = Math true(Math.random()*10)
+
+// 3 SAVOL
+// let a = 12.510
+// console.log(Math.floor(a));
+
+// 4 SAVOL
+// let a = prompt("YOSHINGIZNI KIRITING")
+// let b = 15
+// if(a,b)(
+//     console.log("KOTTA YOSHDASIZ");
+// )else(
+//     console.log("KICHKINASIZ");
+// )
